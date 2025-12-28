355e9e22129effd93cda13fbe5499166b96121c9
radhika pawar <pradhika528@gmail.com>
2025-12-28 20:37:10 +0530

    Ready for Deployment

---
+++ Backend/src/app.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+
+import mongoose from 'mongoose';
+import { connectToSocket } from './controllers/SocketManager.js';
+
+import cors from 'cors';
+import userRoutes from './routes/users.routes.js';
+
+
+const app= express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", (process.env.PORT || 8000));
+app.use(cors());
+app.use(express.json({limit: '40kb'}));
+app.use(express.urlencoded({limit: '40kb', extended: true }));
+
+app.use("/api/v1/users", userRoutes);
+
+
+const start = async() => {
+  app.set("mongo_user")
+  const connectionDb = await mongoose.connect("mongodb+srv://radhikaap06_db_user:RADHERADHE@cluster0.pf1sj98.mongodb.net/")
+
+  console.log(`MONGO Connected DB host : ${connectionDb.connection.host}`);
+
+  server.listen(app.get("port"), () => {
+    console.log("LISTENIN ON PORT 8000");
+  });
+
+}
+start();
---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true},
+    username: {
+    type: String,
+    required: true,
+    unique: true},
+    password: {
+    type: String,
+    required: true},
+    token: {
+      type: String}
+   
+}
+)
+const User =mongoose.model("User", UserSchema);
+export  {User};
